47b5b5f9530efa99e290e68b8406b77bbf1f62d4
James Kong <fishkingsin@gmail.com>
2017-10-14 12:24:49 +0800

    Fixed example-empty

--- examples/example-empty/src/main.cpp
+++ examples/example-empty/src/main.cpp
@@ -1,29 +1,13 @@
-// 
-// main.cpp - RAMDanceToolkit
-// 
-// Copyright 2012-2013 YCAM InterLab, Yoshito Onishi, Satoru Higa, Motoi Shimizu, and Kyle McDonald
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//    http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+#include "ofMain.h"
+#include "ofApp.h"
 
-#include "testApp.h"
-#include "ofAppGlutWindow.h"
+//========================================================================
+int main( ){
+	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
 
-//--------------------------------------------------------------
-int main()
-{
-	ofAppGlutWindow window; // create a window
-	
-	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
-	ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
-	ofRunApp(new testApp); // start the app
+	// this kicks off the running of my app
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
+	ofRunApp(new ofApp());
+
 }
