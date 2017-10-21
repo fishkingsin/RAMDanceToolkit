75c862759b7debc2b49b564e2c1629d70d44e0cd
James Kong <fishkingsin@gmail.com>
2017-10-21 12:28:18 +0800

    UpdateOPenOSC proejct

--- apps/OpenNIOSC/src/main.cpp
+++ apps/OpenNIOSC/src/main.cpp
@@ -19,7 +19,6 @@
 #include "ofAppGlutWindow.h"
 
 int main() {
-	ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 640, 480, OF_WINDOW);
+	ofSetupOpenGL(640, 480, OF_WINDOW);
 	ofRunApp(new ofApp());
 }
